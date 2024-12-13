bacaeafdd31b88682c1fcd3e768702c5ef1006e0
Administrator <Administrator@DESKTOP-1ULGF16>
2024-12-14 00:38:58 +0200

    Add project files.

---
+++ FizzBuzz Exercise 2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FizzBuzz_Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i = 1;
+            
+            for ( i = 1; i <= 15; i++) 
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBUzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if ((i % 5) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
